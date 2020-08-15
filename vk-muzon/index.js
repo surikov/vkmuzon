b71d76dfbee62e8491ec66113c523b54c208b9da
Srgy Surkv <sss1024@gmail.com>
2020-08-15 18:30:14 +0300

    63

--- vk-muzon/index.js
+++ vk-muzon/index.js
@@ -1080,7 +1080,7 @@ RiffShareFlat.prototype.init = function () {
 	}
 
 	];*/
-	this.setupInput();
+	
 	/*
 	window.onresize = function () {
 		riffshareflat.resetSize();
@@ -1133,53 +1133,66 @@ RiffShareFlat.prototype.init = function () {
 	console.log(t);
 	this.storeTracks = [];
 	}*/
-	var AudioContextFunc = window.AudioContext || window.webkitAudioContext;
-	this.audioContext = new AudioContextFunc();
-	this.player = new WebAudioFontPlayer();
-	//this.player.afterTime = 0.1;
-	this.master = new WebAudioFontChannel(this.audioContext);
-	this.echoOn = false;
-	try {
-		var usrAgnt = navigator.userAgent || navigator.vendor || window.opera;
-		if (/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows ce|xda|xiino|android|ipad|playbook|silk/i //
-			.test(usrAgnt) //
-			||
-			/1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i //
-				.test(usrAgnt.substr(0, 4))) {
-			this.echoOn = false;
-			document.title = 'Mobile VKMuzOn';
-		} else {
-			this.echoOn = true;
-			document.title = 'Desktop VKMuzOn';
-		}
-	} catch (noch) {
-		console.log(noch);
-	}
-	this.echoOn = true;
-	if (this.echoOn) {
-		//console.log('init WebAudioFontReverberator');
-		this.reverberator = new WebAudioFontReverberator(this.audioContext);
-		this.reverberator.output.connect(this.audioContext.destination);
-		this.master.output.connect(this.reverberator.input);
+
+	if (this.audioContext) { 
+		//
 	} else {
-		//console.log('skip WebAudioFontReverberator');
-		this.master.output.connect(this.audioContext.destination);
+		this.setupInput();
+		var AudioContextFunc = window.AudioContext || window.webkitAudioContext;
+		this.audioContext = new AudioContextFunc();
+		this.player = new WebAudioFontPlayer();
+		//this.player.afterTime = 0.1;
+		this.master = new WebAudioFontChannel(this.audioContext);
+		this.echoOn = false;
+		try {
+			var usrAgnt = navigator.userAgent || navigator.vendor || window.opera;
+			if (/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows ce|xda|xiino|android|ipad|playbook|silk/i //
+				.test(usrAgnt) //
+				||
+				/1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i //
+					.test(usrAgnt.substr(0, 4))) {
+				this.echoOn = false;
+				document.title = 'Mobile VKMuzOn';
+			} else {
+				this.echoOn = true;
+				document.title = 'Desktop VKMuzOn';
+			}
+		} catch (noch) {
+			console.log(noch);
+		}
+		this.echoOn = true;
+		if (this.echoOn) {
+			//console.log('init WebAudioFontReverberator');
+			this.reverberator = new WebAudioFontReverberator(this.audioContext);
+			this.reverberator.output.connect(this.audioContext.destination);
+			this.master.output.connect(this.reverberator.input);
+		} else {
+			//console.log('skip WebAudioFontReverberator');
+			this.master.output.connect(this.audioContext.destination);
+		}
+		for (var i = 0; i < 8; i++) {
+			this.trackInfo[i].audioNode = this.audioContext.createGain();
+			this.trackInfo[i].audioNode.connect(this.master.input);
+			this.drumInfo[i].audioNode = this.audioContext.createGain();
+			this.drumInfo[i].audioNode.connect(this.master.input);
+		}
+		for (var i = 0; i < this.drumInfo.length; i++) {
+			this.player.adjustPreset(this.audioContext, this.drumInfo[i].sound);
+		}
+		for (var i = 0; i < this.trackInfo.length; i++) {
+			this.player.adjustPreset(this.audioContext, this.trackInfo[i].sound);
+		}
+
+		setInterval(riffshareflat.moveBeatCounter, 100);
+
 	}
-	for (var i = 0; i < 8; i++) {
-		this.trackInfo[i].audioNode = this.audioContext.createGain();
-		this.trackInfo[i].audioNode.connect(this.master.input);
-		this.drumInfo[i].audioNode = this.audioContext.createGain();
-		this.drumInfo[i].audioNode.connect(this.master.input);
-	}
-	for (var i = 0; i < this.drumInfo.length; i++) {
-		this.player.adjustPreset(this.audioContext, this.drumInfo[i].sound);
-	}
-	for (var i = 0; i < this.trackInfo.length; i++) {
-		this.player.adjustPreset(this.audioContext, this.trackInfo[i].sound);
-	}
+
+
+
+
 	this.resetSize();
 	//setInterval(riffshareflat.moveCounter, 100);
-	setInterval(riffshareflat.moveBeatCounter, 100);
+
 	this.loadState();
 
 	//console.log('done init');
@@ -2153,7 +2166,7 @@ RiffShareFlat.prototype.addSmallTiles = function (left, top, width, height) {
 			//console.log('share', url);
 			//bridge.send("VKWebAppShowWallPostBox", { "message": "Hello!" });
 			//vkBridge.send('VKWebAppShowWallPostBox', {"message": "Открыть в VKMuzOn " + url})
-			/*vkBridge.send('VKWebAppShare', { "link": url })
+			vkBridge.send('VKWebAppShare', { "link": url })
 				.then(data => {
 					console.log('vkBridge data', data);
 					riffshareflat.init();
@@ -2161,9 +2174,9 @@ RiffShareFlat.prototype.addSmallTiles = function (left, top, width, height) {
 				.catch(error => {
 					console.log('vkBridge error', error);
 					riffshareflat.init();
-				});*/
+				});
 
-			vkDoShareURL(url);
+			//vkDoShareURL(url);
 		});
 		/*
 				this.tileCircle(g, 13 * this.tapSize, 17 * this.tapSize, 1 * this.tapSize, modeDrumShadow(this.bgMode));
